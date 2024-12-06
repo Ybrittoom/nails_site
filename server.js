0366566800930855da64a61c5548d61ac4a3f965
Yago Emanuel <yagoemanuel@gmail.com>
2024-12-06 19:37:54 -0300

    ....

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+
+// Configurar o diretório para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/nailsmv', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}/nailsmv`)
+})
