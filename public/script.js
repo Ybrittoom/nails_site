0e600d1722329cc20ba6cf4dce7d4bb37dfbbc7d
Yago Emanuel <yagoemanuel@gmail.com>
2024-12-30 20:09:02 -0300

    ..

--- public/script.js
+++ public/script.js
@@ -9,15 +9,32 @@ function chamarAssistente() {
 
 
     setTimeout(() => {
-        textoModalMivy.textContent = "Eu sou a Assistente Mivy"
+        textoModalMivy.textContent = "Eu sou a Assistente Mivy🙋‍♀️"
         falaMivy2()
-    }, 3000);
+    }, 2000);
 
     function falaMivy2() {
         setTimeout(() => {
         memoji.src = 'image/Untitled image.png'
         textoModalMivy.textContent = "Estou aqui para tirar suas duvidas e te ajudar ficar mais linda✨"
-    }, 3000);
+        falaMivy3()
+    }, 2000);
+    }
+
+    function falaMivy3() {
+        setTimeout(() => {
+            memoji.src = 'image/memoji3.png'
+            textoModalMivy.textContent = "Na MV preservamos a beleza, cuidado e manutençao da unhas de nossas clientes💅" 
+        }, 3000)
+    }
+
+    //funçao pra gerar as FAQ la q eu falei no html, se tu tivesse prestado atençao saberia ne INSETO
+    function gerarFaq() {
+        const faqContainer = document.getElementById('faq-container')
+        const faqsServicios = [
+            { pergunta: "Vocês fazem decoração com pedras?", resposta: "Sim! Pedrarias incríveis para deixar suas unhas únicas. 💎"},
+            { pergunta: ""}
+        ]
     }
     
 }
